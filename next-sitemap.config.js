8af0fc6368764d78aa5e4081f58b4e074b83800e
Ighomena Daniel Odebala <ighoodes@gmail.com>
2025-02-22 19:15:52 +0000

    Add packages and styles

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteUrl: "https://magnabytesolutions.co.uk",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "monthly",
+  priority: {
+    default: 0.7,
+    "/": 1.0,
+    "/services": 0.8,
+    "/portfolio": 0.8,
+  },
+};
